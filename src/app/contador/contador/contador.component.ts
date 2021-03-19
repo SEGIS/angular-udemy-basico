3476fac650e98db39f1064f34c5fe38ada4f1efe
david.costo <david.costo@euigs.com>
2021-03-19 20:19:55 +0100

    primer commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,31 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-contador',
+    template: `
+             <h1>{{titulo}}</h1>
+            <!-- <h3>{{1+1}}</h3> -->
+
+            <h3>La base es: <strong> {{base}} </strong></h3>
+
+            <!-- <button (click)=" numero = numero +1; "> +1</button> -->
+            <button (click)="acumular(base)"> + {{base}}</button>
+
+            <span>{{numero}}</span>
+
+            <!-- <button (click)= "restar()"> -1</button> -->
+            <button (click)= "acumular(-base)"> - {{base}}</button>    
+    `   
+})
+
+export class ContadorComponent {
+    titulo: string = 'Contador App';
+    numero: number = 10;
+    base: number = 5;
+  
+    acumular (valor : number){
+      this.numero += valor;
+    }
+}
